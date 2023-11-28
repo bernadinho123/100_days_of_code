c3daea184afc1d01298928c9ba321723553fb26f
Bernardo <bemascarenhas6@gmail.com>
2023-11-28 14:32:59 -0300

    first commit of the day

---
+++ day_2/concatenetion.py
@@ -0,0 +1,9 @@
+num_char=len(input("What is your name? "))
+print("your name has",num_char,"characters")
+#or
+new_num_char=str(num_char)#str converte o valor em uma string
+print("your name has "+new_num_char+" characters")
+
+#a funcao float(valor) converte o uma string ou um interger ou etc em valor float
+#a funcao type(valor) devolve a classe que o valor é, como float, interger e ou string
+print(123[0])
---
+++ day_2/subscript.py
@@ -0,0 +1,6 @@
+print('hello'[0])# é possivel printar uma parte especifica da string
+print('1234'[0])
+
+#integer
+
+print(123_456_789)
