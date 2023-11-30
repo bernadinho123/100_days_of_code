316a5c8aacb5274feec6872f1dad56686815dbe8
Bernardo <bemascarenhas6@gmail.com>
2023-11-30 14:42:00 -0300

    first of the day

---
+++ day_4/list.py
@@ -0,0 +1,3 @@
+# assim se faz uma lista em python = states_of_america = ["oregon","new york"]
+# states_of_america.append("South Dakota") o método append adiciona um elemento ao final da lista
+#lista.extend(["",""]) adiciona uma lista ao final da lista
---
+++ day_4/ramdomisation.py
@@ -0,0 +1,7 @@
+import random
+#import my_module dessa forma consigo importar os dados de outro arquivo
+random_interger = random.randint(1,10)
+print(random_interger)
+#print(my_module.pi) dessa forma consigo usar uma constante definida em my_modules
+random_float = random.random() *5 # ao multiplicar voce expande a range do random, que iria de 0 ate 0.9... para 0 ate 4.9..
+print(random_float)
