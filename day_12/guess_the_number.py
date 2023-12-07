00088616bb3cfe7ff88238404e63494771ef3a50
Bernadinho123 <bemascarenhas6@gmail.com>
2023-12-07 15:46:55 -0300

    day_12

---
+++ day_12/guess_the_number.py
@@ -0,0 +1,57 @@
+import random
+
+
+
+flag = True
+while flag == True:
+
+    print("Welcome to the Number Guessing Game!")
+    level = input("Choose a difficulty. Type 'easy' or 'hard': ")
+    attempts = 0 
+    if level == 'easy':
+        attempts = 10
+        number = random.randint(1,100)
+        while attempts >=1:
+            print(F"You have {attempts} remaining to guess the number.")
+            guess = int(input("Make a guess: "))
+            if guess == number:
+                print(f"You got it! The answer was {number}.")
+                attempts = 0
+            elif guess > number :
+                attempts -=1
+                if attempts != 0:
+                    print("Too High\nGuess again")
+                else:
+                    print("You`ve run out of guesses.You lose!") 
+            else:
+                attempts -=1
+                if attempts != 0:
+                    print("Too Low\nGuess again")
+                else:
+                    print("You`ve run out of guesses.You lose!") 
+    elif level == 'hard':
+        attempts = 5 
+        number = random.randint(1,100)
+        while attempts >=1:
+            print(F"You have {attempts} remaining to guess the number.")
+            guess = int(input("Make a guess: "))
+            if guess == number:
+                print(f"You got it! The answer was {number}.")
+                attempts = 0
+            elif guess > number :
+                attempts -=1
+                if attempts != 0:
+                    print("Too High\nGuess again")
+                else:
+                    print("You`ve run out of guesses.You lose!") 
+            else:
+                attempts -=1
+                if attempts != 0:
+                    print("Too Low\nGuess again")
+                else:
+                    print("You`ve run out of guesses.You lose!") 
+              
+    else:
+        print("Type again ! U gotta chose 'easy' or 'hard'!")
+
+
